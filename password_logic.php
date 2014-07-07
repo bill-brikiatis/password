6349ed655b013dd47c3267ea82a339ee2a1459b5
bill-brikiatis <brikiatis@g.harvard.edu>
2014-07-07 15:46:12 -0400

    Loop array and checkbox logic

--- password_logic.php
+++ password_logic.php
@@ -20,31 +20,58 @@ $word_list = array(	'run',
 	return $word_list;
 }
 
-// Selects a word from array, generates random number
+// Selects a word from array, generates random number for array index, takes in number of words and loop index
 
-function selectWord() {
+function selectWord($number_words, $word_index) {
 	$word_list = wordList();
 	$number_index = count($word_list);
-	$random_number = rand(0, $number_index);
-	$word = ($word_list[$random_number]);
+	$random_number = rand(0, ($number_index - 1));
+	$word = $word_list[$random_number];
+	$checked_dash = 'on';
+	if(($checked_dash == 'on') && (($number_words - 1)!= $word_index)) {
+		$word = $word . "-";
+	}
+	
 	return $word;
 }
 
-// Start Logic
-
-$number_words = 3;
-$checked_dash = 'on';
-$word_list = wordList();
-if(!(is_array($word_list)) || $word_list == NULL) {
-	echo 'Something is wrong. Array of NOT available.';
-	die;
-}
-
-foreach ($word_list as $word_index => $$word_value) {
-	if($word_index >= $number_words) {
-	$word = selectWord();
-	echo $word;
+function loopArray() {
+	$number_words = 5;
+	$word_list = wordList();
+	if(!(is_array($word_list)) || $word_list == NULL) {
+		echo 'Something is wrong. Array of NOT available.';
+		die;
+	}
+	
+	foreach ($word_list as $word_index => $$word_value) {
+		if($word_index < $number_words) {
+			$word = selectWord($number_words, $word_index);
+			echo $word;
+		}
 	}
 }
+
+
+
+// Start checkbox logic
+
+$word = loopArray();
+$check_number = 'off';
+$numbers = array(0, 1, 2, 3, 4, 5, 6, 7, 8, 9);
+$numeral_index = count($numbers);
+$random_numeral = rand(0, ($numeral_index - 1));
+if($check_number == 'on'){
+	$word .= $numbers[$random_numeral];
+}
+
+$check_special = 'on';
+$special = array('~', '@', '#', '%', '^', '&', '*', '+');
+$special_index = count($special);
+$random_special = rand(0, ($special_index - 1));
+if($check_special == 'on'){
+	$word .= $special[$random_special];
+}
+
+echo $word;
 
 ?>
