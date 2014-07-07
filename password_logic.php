424619aaf2bc251ac06721efcdd75fddd12e036f
bill-brikiatis <brikiatis@g.harvard.edu>
2014-07-07 10:40:59 -0400

    Started wordList(), modified selectWord(), modified logic.

--- password_logic.php
+++ password_logic.php
@@ -1,30 +1,50 @@
 <?php
 
-function selectWord() {
-	$word_list = array(	'run', 
-						'walk',
-						'talk',
-						'help',
-						'mister',
-						'friend',
-						'type',
-						'house',
-						'condo',
-						'beach'
+
+// Creates array of words
+
+function wordList() {
+
+$word_list = array(	'run', 
+					'walk',
+					'talk',
+					'help',
+					'mister',
+					'friend',
+					'type',
+					'house',
+					'condo',
+					'beach'
 	);
 	
-	if($word_list) {
-		$number_words = count($word_list);
-		$random_number = rand(0, $number_words);
-	}
-	else {
-		echo 'Something is wrong. Array of NOT available.';
-	}
-	
-	return $word_list[$random_number];
-
+	return $word_ist;
 }
 
-echo selectWord();
+// Selects a word from array, generates random number
 
+function selectWord() {
+	$word_list = wordList();
+	$number_index = count($word_list);
+	$random_number = rand(0, $number_index);
+	$word = ($word_list[$random_number]);
+	return $word;
+}
 
+// Start Logic
+
+$number_words = 3;
+$checked_dash = 'on';
+$word_list = wordList();
+if(!(is_array($word_list)) || $word_list == NULL) {
+	echo 'Something is wrong. Array of NOT available.';
+	die;
+}
+
+foreach ($word_list as $word_index => $$word_value) {
+	if($word_index >= $number_words) {
+	$word = selectWord();
+	echo $word;
+	}
+}
+
+?>
